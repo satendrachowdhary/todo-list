769a6f30fc2498474d891c97775bcc691802bbde
satendra nath chowdhary <53505496+satendrachowdhary@users.noreply.github.com>
2019-10-19 19:30:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+var li = document.querySelectorAll("li");
+
+for (let i = 0; i < li.length; i++) {
+  var del = document.createElement("button");
+  del.textContent = "Delete";
+  li[i].appendChild(del);
+  del.addEventListener("click", () => {
+    li[i].remove();
+  });
+}
+
+ul.addEventListener("click", event => {
+  event.target.classList.toggle("done");
+});
+
+function inputLength() {
+  return input.value.replace(/\s/g, "").length > 0;
+}
+
+function createListElement() {
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode(input.value));
+  var del = document.createElement("button");
+  del.textContent = "Delete";
+  li.appendChild(del);
+  del.addEventListener("click", () => {
+    li.remove();
+  });
+  ul.appendChild(li);
+  input.value = "";
+}
+
+function addListAfterClick() {
+  if (inputLength()) {
+    createListElement();
+  }
+}
+
+function addListAfterKeypress(event) {
+  if (inputLength() && event.keyCode === 13) {
+    createListElement();
+  }
+}
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
